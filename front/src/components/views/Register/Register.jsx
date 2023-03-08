4e86fe698c51c45720bbd80a670c176932cf4a79
Jairo Naranjo <jairo_naranjo80111@hotmail.com>
2023-03-08 13:00:14 +1000

    avances del registro

--- front/src/components/views/Register/Register.jsx
+++ front/src/components/views/Register/Register.jsx
@@ -1,114 +1,71 @@
 import React, { useState } from 'react'
+import { Link,  } from "react-router-dom";
 import axios from 'axios'
 import "./Register.css"
 import logo1 from "../../../assets/logo1.png"
 import Swal from 'sweetalert2'
+import { postUser } from '../../../redux/actions/index'
+import { useDispatch, useSelector } from "react-redux";
+
+function validate(input) {
+    let errors = {}
+    if (!input.name) {
+        errors.name = "Nombre del club es requerido"
+    }
+    else if (!input.email) {
+        errors.email = "Email es requerido"
+    }
+    else if (!input.password) {
+        errors.password = "Contraseña es requerida"
+    }
+    else if (input.password2 != input.password2) {
+        errors.password2 = "Las contraseñas deben ser iguales"
+    }
+    return errors
+}
 
 const Register = () => {
-    const [errors, setErrors] = useState({});
-    const [body, setBody] = useState({
-        name: '',
-        username: '',
-        typeuser: '',
-        gender: '',
-        email: '',
-        password: '',
-        confirmpassword: '',
-        terminos: '',
-        edad: ''
+    const dispatch = useDispatch()
+    // const allTemps = useSelector((state) => state.temps)
+    const [errors, setErrors] = useState({})
+
+    const [input, setInput] = useState({
+        name: "",
+        email: "",
+        password: "",
+        password2: ""
     })
 
-    function validate(body) {
-        const expresiones = {
-            nombre: /^[a-zA-ZÀ-ÿ\s]{1,40}$/, // Letras y espacios, pueden llevar acentos.
-            password: /^(?=\w*\d)(?=\w*[A-Z])(?=\w*[a-z])\S{8,16}$/, // tener al entre 8 y 16 caracteres, al menos un dígito, al menos una minúscula, al menos una mayúscula y al menos un caracter no alfanumérico.
-            celular: /^\(?(\d{3})\)?[-]?(\d{3})[-]?(\d{4})$/,
-            correo: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/,
-        };
-        const errors = {};
-        if (!expresiones.nombre.test(body.name)) {
-            errors.name =
-                "Debe ingresar un nombre válido (no se permiten números ni símbolos)";
-        } else if (!expresiones.correo.test(body.email)) {
-            errors.email = "Debe ingresar un correo válido (nombre@proveedor.com)";
-        } else if (!expresiones.password.test(body.password)) {
-            errors.password =
-                "8 y 16 caracteres, minimo un número, minimo una minúscula, minimo una mayúscula.";
-        } else if (body.password !== body.confirmpassword) {
-            errors.confirmpassword = "Las contraseñas deben ser iguales.";
-        } else if (body.typeuser === '') {
-            errors.typeuser = "Seleccione tipo de usuario.";
-        } else if (body.gender === '') {
-            errors.gender = "Seleccione genero.";
-        } else if (body.terminos === '') {
-            errors.terminos = "Debe Aceptar Terminos y Condiciones.";
-        } else if (body.edad === '') {
-            errors.edad = "Debe ser mayor de edad.";
-        }
-        return errors;
-    }
-
-
-    const inputChange = ({ target }) => {
-        const { name, value } = target
-        setBody({
-            ...body,
-            [name]: value
+    function handleChange(e) {
+        setInput({
+            ...input,
+            [e.target.name]: e.target.value
         })
-        setErrors(
-            validate({
-                ...body,
-                [name]: value
-            })
-        );
+        setErrors(validate({
+            ...input,
+            [e.target.name]: e.target.value
+        }))
     }
 
-    const onSubmit = () => {
-        console.log(body)
-        if (
-            body.name === "" ||
-            body.username === "" ||
-            // body.typeuser === "" ||
-            // body.gender === "" ||
-            body.email === "" ||
-            body.password === "" ||
-            body.confirmpassword === "" ||
-            body.terminos === "" ||
-            body.edad === ""
-        ) {
-            Swal.fire({
-                icon: 'error',
-                title: 'Oops...',
-                text: 'Debe completar todos los campos!'
-            })
-        } else if (
-            errors.name ||
-            // errors.typeuser ||
-            // errors.gender ||
-            errors.email ||
-            errors.password ||
-            errors.confirmpassword ||
-            errors.terminos ||
-            errors.edad
-        ) {
-            Swal.fire({
-                icon: 'error',
-                title: 'Oops...',
-                text: 'Debe completar todos los campos!'
-            })
-        } else {
-            axios.post('http://localhost:4000/api/register', body)
-                .then(({ data }) => {
-                    console.log(data);
-                })
-                .catch(({ response }) => {
-                    console.log(response)
-                })
+    function handleSubmit(e) {
+        if (errors.name || errors.email || errors.password || errors.password2 ) {
+            alert('Debe completar todos los campos')
         }
+        else {
+            e.preventDefault()
+            console.log(input)
+            dispatch(postUser(input))
+            alert("Usuario creado con exito")
+            setInput({
+                name: "",
+                email: "",
+                password: "",
+                password2: ""
+            })
 
+        }
     }
-
-
+    
 
     return (
         <>
@@ -125,8 +82,8 @@ const Register = () => {
                                             <input
                                                 id='name'
                                                 type="text"
-                                                value={body.name}
-                                                onChange={inputChange}
+                                                value={input.name}
+                                                onChange={(e) => handleChange(e)}
                                                 name='name'
                                                 placeholder="Nombre del club" />
                                         </div>
@@ -137,8 +94,8 @@ const Register = () => {
                                             <input
                                                 id='email'
                                                 type="email"
-                                                value={body.email}
-                                                onChange={inputChange}
+                                                value={input.email}
+                                                onChange={(e) => handleChange(e)}
                                                 name='email'
                                                 placeholder="Correo Electronico" />
                                         </div>
@@ -149,8 +106,8 @@ const Register = () => {
                                             <input
                                                 id='password'
                                                 type="password"
-                                                value={body.password}
-                                                onChange={inputChange}
+                                                value={input.password}
+                                                onChange={(e) => handleChange(e)}
                                                 name='password'
                                                 placeholder="Contraseña" />
                                         </div>
@@ -161,64 +118,17 @@ const Register = () => {
                                             <input
                                                 id='confirmpassword'
                                                 type="password"
-                                                value={body.confirmpassword}
-                                                onChange={inputChange}
-                                                name='confirmpassword'
+                                                value={input.confirmpassword}
+                                                onChange={(e) => handleChange(e)}
+                                                name='password2'
                                                 placeholder="Repetir Contraseña" />
                                         </div>
-                                        {errors.confirmpassword && (
-                                            <p className="errosRegistro">{errors.confirmpassword}</p>
+                                        {errors.password2 && (
+                                            <p className="errosRegistro">{errors.password2}</p>
                                         )}
-                                        <br />
-                                        {/* <label className='textfff'>Tipo de usuario</label> */}
-                                        {/* <div className='mb-3'>
-                                                <p className='radio_options'><input
-                                                    type="radio"
-                                                    className='textfff'
-                                                    label="Cliente"
-                                                    name="typeuser"
-                                                    value="1" onChange={inputChange} /> Cliente</p>
-                                                <p className='radio_options'><input
-                                                    type="radio"
-                                                    className='textfff'
-                                                    inline
-                                                    label="Tatuador"
-                                                    name="typeuser"
-                                                    value="2" onChange={inputChange} /> Tatuador</p>
-                                            
-                                        </div> */}
-                                        {/* {errors.typeuser && (
-                                            <p className="errosRegistro">{errors.typeuser}</p>
-                                        )} */}
-
-                                        {/* <label className='textfff'>Genero</label>
-                                        <div className='mb-3'>
-                                                <p className='radio_options'><input
-                                                type="radio"
-                                                    className='textfff'
-                                                    label="Masculino"
-                                                    name="gender"
-                                                    value="1" onChange={inputChange} /> Masculino</p>
-                                                <p className='radio_options'><input
-                                                    type="radio"
-                                                    className='textfff'
-                                                    label="Femenino"
-                                                    name="gender"
-                                                    value="2" onChange={inputChange} /> Femenino</p>
-                                                <p className='radio_options'><input
-                                                    type="radio"
-                                                    className='textfff'
-                                                    label="Otro"
-                                                    name="gender"
-                                                    value="3" onChange={inputChange} /> Otro</p>
-                                            
-                                        </div>
-                                        {errors.gender && (
-                    <p className="errosRegistro">{errors.gender}</p>
-                  )} */}
                   <br/>
 
-                  <div className="mb-3">
+                  {/* <div className="mb-3">
                                         <label className='textfff alinear_left'>Al registrarse estas aceptando los Términos y Condiciones y Politicas de Privacidad</label>
                                         <div className='d-flex'>
                                         <p className='textfff aceptar'><input type="checkbox" onChange={inputChange} name='terminos' /> Aceptar</p>
@@ -237,10 +147,10 @@ const Register = () => {
                                         {errors.edad && (
                     <p className="errosRegistro">{errors.edad}</p>
                   )}
-                                    </div>
+                                    </div> */}
                   <br/>
 
-                                        <a className='iniciar_sesion' onClick={onSubmit} value="Register">
+                                        <a className='iniciar_sesion' onSubmit={(e) => handleSubmit(e)} value="Register">
                                             <span></span>
                                             <span></span>
                                             <span></span>
@@ -251,6 +161,7 @@ const Register = () => {
                                 </div>
 
                             </div>
+                            <button class="btn-create-dog">Create Dog</button>
                         </form>
                     </div>
                 </div>
--- front/src/redux/actions/index.js
+++ front/src/redux/actions/index.js
@@ -1,44 +1,12 @@
-export const GET_TATUADORES = 'GET_TATUADORES';
-export const FILTER_BY_COUNTRY = 'FILTER_BY_COUNTRY';
-export const ALL_TATUADORES = 'ALL_TATUADORES';
-export const FILTER_BY_CITY = 'FILTER_BY_CITY';
-export const FILTER_BY_STYLE = 'FILTER_BY_STYLE';
+
 
 
 const axios = require('axios');
 
 
-export const getTatuadores = () => (dispatch) => {
-    return fetch("")
-    .then((response) => response.json())
-    .then((json) => {dispatch({ type: GET_TATUADORES, payload:json})})
-};
-
-
-export function paisFilter(value){
-    return{
-        type: FILTER_BY_COUNTRY,
-        payload: value
+export function postUser (payload){
+    return async function (dispatch){
+        const response = await axios.post("http://localhost:4000/users/register", payload)
+        return response
     }
 }
-
-export function ciudadFilter(value){
-    return{
-        type: FILTER_BY_CITY,
-        payload: value
-    }
-}
-
-export function estiloFilter(value){
-    return{
-        type: FILTER_BY_STYLE,
-        payload: value
-    }
-}
-
-export function todos(value){
-    return{
-        type: ALL_TATUADORES,
-        payload: value
-    }
-  }
